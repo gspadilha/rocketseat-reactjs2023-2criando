43e22d777706bb985137d4c5a478946e513dcf24
Guilherme Padilha <gs.padilha@gmail.com>
2023-05-14 11:41:04 -0300

    feat: início do desenvolvimento

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BsStopwatch } from 'react-icons/bs';
+import { IoReceiptOutline } from 'react-icons/io5';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className='h-[calc(100vh-5rem)] w-[70rem] rounded-[8px] bg-zinc-800'>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <span>LOGO</span>
+        <span>
+          <BsStopwatch size={25} aria-label='button-cronometro' />
+          <IoReceiptOutline size={25} aria-label='button-registro' />
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div>app</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
